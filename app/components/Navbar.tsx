0b1726a41cc181edb3f6f1d4bdb7a156c1eeff5b
maithilysawant <maithily1803@gmail.com>
2025-09-21 05:43:21 +0530

    auth

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router"
+
+const navbar = () => {
+  return (
+    <nav className="navbar">
+        <Link to="/">
+        <p className="text-2xl font-bold text-gradient">JOBFIT</p>
+        </Link>
+        <Link to="/uplad" className="primary-button w-fit">
+        Upload Resume
+        </Link>
+    </nav>
+  )
+}
+
+export default navbar
